401b5e2eeb3c3be5e7ec4e97d6d54e08beee45ff
Daniel Bos <daniel.bos@sap.com>
2024-05-15 13:46:47 +0800

    chore: add unit-test coverage

---
+++ cmd/unwise/app_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain_newApp(t *testing.T) {
+	rq := require.New(t)
+
+	server := newServer()
+	app := newApp(server)
+
+	rq.NotNil(app)
+
+	routes := make(map[string]struct{})
+
+	for _, route := range app.GetRoutes(true) {
+		routes[route.Path] = struct{}{}
+	}
+
+	rq.Contains(routes, "/api/v2/auth")
+	rq.Contains(routes, "/api/v2/highlights")
+	rq.Contains(routes, "/api/v2/books")
+}
